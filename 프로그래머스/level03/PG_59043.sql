d1f3472fdd60c871bbc3f3267f2221c1bb25c295
공대표 <c5ngsdp@gmail.com>
2024-07-03 23:47:03 +0900

    SQL

---
+++ 프로그래머스/level03/PG_59043.sql
@@ -0,0 +1,11 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/59043 */
+
+SELECT
+    A.ANIMAL_ID,
+    A.NAME
+FROM
+    ANIMAL_INS A
+INNER JOIN
+    ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID AND A.DATETIME > B.DATETIME
+ORDER BY
+    A.DATETIME ASC
